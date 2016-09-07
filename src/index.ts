b1da0d987acb24f31948e5685b61b0d683aedac2
Ivo Stratev <ivo.stratev.tues@gmail.com>
2016-09-08 01:19:41 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 export function bind<T extends Function>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> | void {
-	return {
-		configurable: false,
-		enumerable: true,
-		get(): T {
-			return descriptor.value.bind(this);
-		}
-	};
+    if(descriptor === undefined) throw new Typeerror(`Only functions can be decorated with @bind. <${propertyKey}> is not a function!`);
+    if(typeof descriptor.value !== 'function') throw new Typeerror(`Only functions can be decorated with @bind. <${propertyKey}> is not a function, called on ${typeof descriptor.value}!`);
+
+    return {
+        configurable: true,
+        get(): T {
+            return descriptor.value.bind(this);
+        }
+    };
 }
 
 export default bind;
