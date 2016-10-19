4b188a4d075e84d7ed1cb052c30f05368da8f73a
NoHomey <ivo.stratev.tues@gmail.com>
2016-10-19 15:21:32 +0300

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
-export function bind<T>(target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> | void {
-    if(!descriptor || (typeof descriptor.value !== 'function')) {
+namespace constants {
+    export const typeOfFunction: 'function' = 'function';
+    export const boolTrue: boolean = true;
+}
+
+export function bind<T extends Function>(target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> | void {
+    if(!descriptor || (typeof descriptor.value !== constants.typeOfFunction)) {
         throw new TypeError(`Only functions can be decorated with @bind. <${propertyKey}> is not a function!`);
     }
     
     return {
-        configurable: true,
+        configurable: constants.boolTrue,
         get(this: T): T {
-            const bound: T = (descriptor.value as any as Function).bind(this);
+            const bound: T = descriptor!.value!.bind(this);
             // Credits to https://github.com/andreypopp/autobind-decorator for memoizing the result of bind against a symbol on the instance.
-            Object.defineProperty(this, propertyKey, { value: bound, configurable: true, writable: true });
+            Object.defineProperty(this, propertyKey, {
+                value: bound,
+                configurable: constants.boolTrue,
+                writable: constants.boolTrue
+            });
             return bound;
         }
     };
