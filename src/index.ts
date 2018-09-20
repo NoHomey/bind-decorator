d5eaf25222096c00b58bf158d300c6187e020683
bisubus <subus.gh@gmail.com>
2018-09-20 17:05:27 +0300

    Remove constants (#9)

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
-namespace constants {
-    export const typeOfFunction: 'function' = 'function';
-    export const boolTrue: boolean = true;
-}
-
 export function bind<T extends Function>(target: object, propertyKey: string, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> | void {
-    if(!descriptor || (typeof descriptor.value !== constants.typeOfFunction)) {
+    if(!descriptor || (typeof descriptor.value !== 'function')) {
         throw new TypeError(`Only methods can be decorated with @bind. <${propertyKey}> is not a method!`);
     }
     
     return {
-        configurable: constants.boolTrue,
+        configurable: true,
         get(this: T): T {
             const bound: T = descriptor.value!.bind(this);
             // Credits to https://github.com/andreypopp/autobind-decorator for memoizing the result of bind against a symbol on the instance.
             Object.defineProperty(this, propertyKey, {
                 value: bound,
-                configurable: constants.boolTrue,
-                writable: constants.boolTrue
+                configurable: true,
+                writable: true
             });
             return bound;
         }
