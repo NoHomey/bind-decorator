48f991a98e3955a2ac32cc9a3b6f2ee5935aa3e6
Grzegorz Osimowicz <grzegorz@osimowicz.eu>
2022-12-14 10:03:06 +0100

    Allow to override bound methods

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-export function bind<T extends Function>(target: object, propertyKey: string, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> | void {
+export function bind<T extends Function>(
+    target: object, 
+    propertyKey: string, 
+    descriptor: TypedPropertyDescriptor<T>
+): TypedPropertyDescriptor<T> | void {
     if(!descriptor || (typeof descriptor.value !== 'function')) {
         throw new TypeError(`Only methods can be decorated with @bind. <${propertyKey}> is not a method!`);
     }
     
     return {
         configurable: true,
+        set(value) {
+            Object.defineProperty(this, propertyKey, {
+                value: value,
+                configurable: true,
+                writable: true
+            });
+        },
         get(this: T): T {
             const bound: T = descriptor.value!.bind(this);
             // Credits to https://github.com/andreypopp/autobind-decorator for memoizing the result of bind against a symbol on the instance.
